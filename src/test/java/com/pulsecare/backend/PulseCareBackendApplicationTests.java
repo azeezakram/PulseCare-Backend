6f14244a87de0945d4c21c7b6febc218f84dfc6f
Abdul Azeez <azeez7045@gmail.com>
2025-11-15 22:29:21 +0530

    Project initialization

---
+++ src/test/java/com/pulsecare/backend/PulseCareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsecare.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseCareBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
