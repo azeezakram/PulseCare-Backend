474f96d885dd1edb1fb28e7160a38efb0b32e4f7
Abdul Azeez <azeez7045@gmail.com>
2025-11-21 01:55:13 +0530

    feat: Add timestamp field to ResponseBody and initialize it with current time

--- src/main/java/com/pulsecare/backend/common/template/response/ResponseBody.java
+++ src/main/java/com/pulsecare/backend/common/template/response/ResponseBody.java
@@ -1,4 +1,10 @@
 package com.pulsecare.backend.common.template.response;
 
-public record ResponseBody<T>(int status, String message, T data) {
+import java.time.LocalDateTime;
+
+public record ResponseBody<T>(int status, String message, T data, LocalDateTime timestamp) {
+    public ResponseBody(int status, String message, T data) {
+        this(status, message, data, LocalDateTime.now());
+    }
 }
+
