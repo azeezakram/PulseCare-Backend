3d1de36a67d89dfbc1fc7aebf3adb06639f2ba99
Abdul Azeez <azeez7045@gmail.com>
2025-11-22 02:44:24 +0530

    feat: Add createdAt and updatedAt timestamps to Role entity

--- src/main/java/com/pulsecare/backend/module/role/model/Role.java
+++ src/main/java/com/pulsecare/backend/module/role/model/Role.java
@@ -3,7 +3,10 @@ package com.pulsecare.backend.module.role.model;
 import com.pulsecare.backend.module.user.model.Users;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Entity
@@ -17,9 +20,18 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false, unique = true)
     private String name;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
     @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
     @ToString.Exclude
     private Set<Users> users;
+
 }
