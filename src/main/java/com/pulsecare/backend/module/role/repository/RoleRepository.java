77e87d7002a2a67ec95780fb896917d77ded6b33
Abdul Azeez <azeez7045@gmail.com>
2025-11-22 02:47:01 +0530

    feat: Create RoleRepository for Role entity data access

---
+++ src/main/java/com/pulsecare/backend/module/role/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.pulsecare.backend.module.role.repository;
+
+import com.pulsecare.backend.module.role.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
