33b5d8fc3d5e6a01860a0d9a32fd05d11cf3103d
Abdul Azeez <azeez7045@gmail.com>
2025-11-22 03:32:52 +0530

    feat: Refactor RoleService to implement service interfaces for better modularity

--- src/main/java/com/pulsecare/backend/module/role/service/RoleService.java
+++ src/main/java/com/pulsecare/backend/module/role/service/RoleService.java
@@ -1,11 +1,11 @@
 package com.pulsecare.backend.module.role.service;
 
-import com.pulsecare.backend.common.base.service.*;
+import com.pulsecare.backend.common.base.service.CreatableService;
+import com.pulsecare.backend.common.base.service.DeletableService;
+import com.pulsecare.backend.common.base.service.FindableService;
+import com.pulsecare.backend.common.base.service.UpdatableService;
 import com.pulsecare.backend.module.role.dto.RoleReqDto;
 import com.pulsecare.backend.module.role.dto.RoleResDto;
-import com.pulsecare.backend.module.user.dto.LoginRequestDTO;
-import com.pulsecare.backend.module.user.dto.UserRequestDTO;
-import com.pulsecare.backend.module.user.dto.UserResponseDTO;
 
 public interface RoleService extends
         FindableService<Integer, RoleResDto>,
