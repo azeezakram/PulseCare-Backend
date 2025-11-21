bfc964bfef2b1225419b72e44f0992e657c6ff99
Abdul Azeez <azeez7045@gmail.com>
2025-11-22 04:18:29 +0530

    feat: Remove "ROLE_" from role name

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,9 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 -- Insert roles
 INSERT INTO role (name, created_at, updated_at)
-VALUES ('ROLE_ADMIN', now(), now()),
-       ('ROLE_DOCTOR', now(), now()),
-       ('ROLE_NURSE', now(), now());
+VALUES ('ADMIN', now(), now()),
+       ('DOCTOR', now(), now()),
+       ('NURSE', now(), now());
 INSERT INTO department (name, created_at, updated_at)
 VALUES ('Cardiology', now(), now()),
        ('Neurology', now(), now()),
