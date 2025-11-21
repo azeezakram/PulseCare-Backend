8cca6ce1e81b5ae926069f9f0ce26da1b6bd93dc
Abdul Azeez <azeez7045@gmail.com>
2025-11-22 03:00:52 +0530

    feat: Implement RoleService methods for finding roles by ID and handling not found exceptions

--- src/main/java/com/pulsecare/backend/module/role/service/RoleServiceImpl.java
+++ src/main/java/com/pulsecare/backend/module/role/service/RoleServiceImpl.java
@@ -1,31 +1,38 @@
 package com.pulsecare.backend.module.role.service;
 
+import com.pulsecare.backend.common.exception.ResourceNotFoundException;
 import com.pulsecare.backend.module.role.dto.RoleReqDto;
 import com.pulsecare.backend.module.role.dto.RoleResDto;
+import com.pulsecare.backend.module.role.mapper.RoleMapper;
+import com.pulsecare.backend.module.role.model.Role;
+import com.pulsecare.backend.module.role.repository.RoleRepository;
+import com.pulsecare.backend.module.specialization.model.Specialization;
 import com.pulsecare.backend.module.user.dto.LoginRequestDTO;
 import com.pulsecare.backend.module.user.dto.UserRequestDTO;
 import com.pulsecare.backend.module.user.dto.UserResponseDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
+    private final RoleRepository repository;
+    private final RoleMapper mapper;
 
-    @Override
-    public RoleResDto create(RoleReqDto data) {
-        return null;
-    }
-
-    @Override
-    public void delete(Integer id) {
-        // TODO document why this method is empty
+    public RoleServiceImpl(RoleRepository repository, RoleMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
     }
 
     @Override
     public RoleResDto findById(Integer id) {
-        return null;
+        Role data = repository.findById(id).orElse(null);
+
+        return Optional.ofNullable(data)
+                .map(mapper::toDTO)
+                .orElseThrow(() -> new ResourceNotFoundException("Role not found"));
     }
 
     @Override
@@ -34,7 +41,16 @@ public class RoleServiceImpl implements RoleService {
     }
 
     @Override
+    public RoleResDto create(RoleReqDto data) {
+        return null;
+    }
+
+    @Override
     public RoleResDto update(Integer integer, RoleReqDto data) {
         return null;
+    }
+    @Override
+    public void delete(Integer id) {
+
     }
 }
