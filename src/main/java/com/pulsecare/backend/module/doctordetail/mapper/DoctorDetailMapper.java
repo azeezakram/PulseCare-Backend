cb087f4d7dd7fb526402a1a06656afe0dc337be4
Abdul Azeez <azeez7045@gmail.com>
2025-11-26 21:41:44 +0530

    refactor(doctor): enhance DoctorDetailMapper with updateEntity method and ignore specific fields

--- src/main/java/com/pulsecare/backend/module/doctordetail/mapper/DoctorDetailMapper.java
+++ src/main/java/com/pulsecare/backend/module/doctordetail/mapper/DoctorDetailMapper.java
@@ -3,13 +3,20 @@ package com.pulsecare.backend.module.doctordetail.mapper;
 import com.pulsecare.backend.module.doctordetail.dto.DoctorDetailReqDto;
 import com.pulsecare.backend.module.doctordetail.dto.DoctorDetailResDto;
 import com.pulsecare.backend.module.doctordetail.model.DoctorDetail;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface DoctorDetailMapper {
+
+    @Mapping(target = "specializations", ignore = true)
+    @Mapping(target = "user", ignore = true)
     DoctorDetail toEntity(DoctorDetailReqDto dto);
 
     @Mapping(source = "user.id", target = "userId")
     DoctorDetailResDto toDTO(DoctorDetail entity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "specializations", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntity(DoctorDetailReqDto dto, @MappingTarget DoctorDetail entity);
 }
