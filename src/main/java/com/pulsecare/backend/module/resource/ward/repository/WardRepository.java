49a40a077cd3672ca7235bd97cf0579b3ca8050c
Abdul Azeez <azeez7045@gmail.com>
2025-11-27 03:45:56 +0530

    feat: add @Repository annotation to DepartmentRepository and WardRepository interfaces

--- src/main/java/com/pulsecare/backend/module/resource/ward/repository/WardRepository.java
+++ src/main/java/com/pulsecare/backend/module/resource/ward/repository/WardRepository.java
@@ -2,9 +2,11 @@ package com.pulsecare.backend.module.resource.ward.repository;
 
 import com.pulsecare.backend.module.resource.ward.model.Ward;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface WardRepository extends JpaRepository<Ward, Integer> {
     Optional<Ward> findByName(String name);
 }
