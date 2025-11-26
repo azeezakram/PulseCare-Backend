17cf7b2ad599ddd74b8a148a202466864ba8152e
Abdul Azeez <azeez7045@gmail.com>
2025-11-26 21:40:38 +0530

    refactor(doctor): simplify DoctorDetailRepository by removing unused methods

--- src/main/java/com/pulsecare/backend/module/doctordetail/repository/DoctorDetailRepository.java
+++ src/main/java/com/pulsecare/backend/module/doctordetail/repository/DoctorDetailRepository.java
@@ -2,8 +2,6 @@ package com.pulsecare.backend.module.doctordetail.repository;
 
 import com.pulsecare.backend.module.doctordetail.model.DoctorDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,12 +9,7 @@ import java.util.UUID;
 
 @Repository
 public interface DoctorDetailRepository extends JpaRepository<DoctorDetail, Long> {
-    Optional<DoctorDetail> findByUserIdAndLicenseNo(UUID userId, String licenseNo);
     Optional<DoctorDetail> findByUserId(UUID userId);
-
-    // Only check licenseNo if it is being changed
-    @Query("SELECT d FROM DoctorDetail d WHERE d.licenseNo = :licenseNo AND d.user.id <> :userId")
-    Optional<DoctorDetail> findByLicenseNoAndUserIdNot(@Param("licenseNo") String licenseNo,
-                                                       @Param("userId") UUID userId);
+    Optional<DoctorDetail> findByLicenseNo(String licenseNo);
 }
 
