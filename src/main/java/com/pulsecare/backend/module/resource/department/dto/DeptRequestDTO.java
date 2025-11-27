6a1d6f0329ed91f0945b4a95534718fd384b2bd5
Abdul Azeez <azeez7045@gmail.com>
2025-11-27 10:31:53 +0530

    feat(dto): refactor DeptRequestDTO and DeptResponseDTO for improved readability

--- src/main/java/com/pulsecare/backend/module/resource/department/dto/DeptRequestDTO.java
+++ src/main/java/com/pulsecare/backend/module/resource/department/dto/DeptRequestDTO.java
@@ -1,8 +1,6 @@
 package com.pulsecare.backend.module.resource.department.dto;
 
-import jakarta.validation.constraints.NotEmpty;
-
 public record DeptRequestDTO(
-        @NotEmpty(message = "Name is required")
-        String name) {
+        String name
+) {
 }
--- src/main/java/com/pulsecare/backend/module/resource/department/dto/DeptResponseDTO.java
+++ src/main/java/com/pulsecare/backend/module/resource/department/dto/DeptResponseDTO.java
@@ -2,5 +2,10 @@ package com.pulsecare.backend.module.resource.department.dto;
 
 import java.time.LocalDateTime;
 
-public record DeptResponseDTO(Integer id, String name, LocalDateTime createdAt, LocalDateTime updatedAt) {
+public record DeptResponseDTO(
+        Integer id,
+        String name,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
 }
