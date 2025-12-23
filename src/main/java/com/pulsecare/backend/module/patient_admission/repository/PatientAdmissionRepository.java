f3c4102e7b7a52637d86b851ca96c0d4f3d9bb6a
Abdul Azeez <azeez7045@gmail.com>
2025-12-23 21:20:20 +0530

    feat(patient_admission): create PatientAdmissionRepository

---
+++ src/main/java/com/pulsecare/backend/module/patient_admission/repository/PatientAdmissionRepository.java
@@ -0,0 +1,9 @@
+package com.pulsecare.backend.module.patient_admission.repository;
+
+import com.pulsecare.backend.module.patient_admission.model.PatientAdmission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientAdmissionRepository extends JpaRepository<PatientAdmission, Long> {
+}
