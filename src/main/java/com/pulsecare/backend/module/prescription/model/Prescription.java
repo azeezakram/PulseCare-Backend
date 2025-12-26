d4702ea60b5e704f0dba67eed72adea21b08611d
Abdul Azeez <azeez7045@gmail.com>
2025-12-26 05:48:38 +0530

    feat(Prescription): add doctor reference and items list to Prescription entity

--- src/main/java/com/pulsecare/backend/module/prescription/model/Prescription.java
+++ src/main/java/com/pulsecare/backend/module/prescription/model/Prescription.java
@@ -4,12 +4,16 @@ import com.pulsecare.backend.module.patient_admission.model.PatientAdmission;
 import com.pulsecare.backend.module.patient_queue.model.PatientQueue;
 import com.pulsecare.backend.module.prescription.enums.PrescriptionStatus;
 import com.pulsecare.backend.module.prescription.enums.PrescriptionType;
+import com.pulsecare.backend.module.resource.ward.model.Ward;
+import com.pulsecare.backend.module.user.model.Users;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "prescription")
@@ -23,6 +27,10 @@ public class Prescription {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id")
+    private Users doctor;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "queue_id")
@@ -42,6 +50,11 @@ public class Prescription {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private PrescriptionStatus status;
+
+    @OneToMany(mappedBy = "prescription", cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY, orphanRemoval = true)
+    @ToString.Exclude
+    private List<PrescriptionItem> items;
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
