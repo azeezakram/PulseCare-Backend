84f79a3bc9d57bb871c8a59648032fb06280fd88
Abdul Azeez <azeez7045@gmail.com>
2025-12-27 18:02:38 +0530

    feat(feat): add findAllByPrescriptionId method in PrescriptionItemRepository

--- src/main/java/com/pulsecare/backend/module/prescription/repository/PrescriptionItemRepository.java
+++ src/main/java/com/pulsecare/backend/module/prescription/repository/PrescriptionItemRepository.java
@@ -4,6 +4,9 @@ import com.pulsecare.backend.module.prescription.model.PrescriptionItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PrescriptionItemRepository extends JpaRepository<PrescriptionItem, Long> {
+    Optional<PrescriptionItem> findAllByPrescriptionId(Long prescriptionId);
 }
